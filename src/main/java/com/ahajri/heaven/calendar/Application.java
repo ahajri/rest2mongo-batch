e924b61c2aff6104ea44aa6d86a1472f1f612b6f
J18JR00 <ahajri.prestataire@si2m.fr>
2018-09-21 17:12:57 +0200

    activate security

--- src/main/java/com/ahajri/heaven/calendar/Application.java
+++ src/main/java/com/ahajri/heaven/calendar/Application.java
@@ -6,30 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class Application /*extends WebSecurityConfigurerAdapter*/
-
-{
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-//	private CsrfTokenRepository csrfTokenRepository() {
-//		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-//		repository.setSessionAttributeName("_csrf");
-//		return repository;
-//	}
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//
-//		http.antMatcher("/**")
-//		.authorizeRequests()
-//		.antMatchers("/**", "/auth**", "/webjars/**").permitAll().anyRequest()
-//				.permitAll();
-//
-//		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll();
-//
-//	}
 
 }
