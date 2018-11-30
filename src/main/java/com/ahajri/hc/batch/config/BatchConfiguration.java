8cccd0102343ee8e7ef04b4c395806ecaadf1a6b
J18JR00 <ahajri.prestataire@si2m.fr>
2018-11-30 14:22:20 +0100

    add batch processing

--- src/main/java/com/ahajri/hc/batch/config/BatchConfiguration.java
+++ src/main/java/com/ahajri/hc/batch/config/BatchConfiguration.java
@@ -132,7 +132,7 @@ public class BatchConfiguration {
 	}
 	// end::jobstep[]
 
-	@Scheduled(cron = "0 22 14 * * *")
+	@Scheduled(cron = "0 24 14 * * *")
 	public void startScandvPrayTimeJob() throws Exception {
 		LOG.info(" ====> Job Started at :" + new Date());
 		JobParameters param = new JobParametersBuilder().addString("JobID", String.valueOf(System.currentTimeMillis()))
