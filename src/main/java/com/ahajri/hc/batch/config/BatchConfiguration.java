aeaa46f88c1dbf63efce3b8b4eb3485f17891830
J18JR00 <ahajri.prestataire@si2m.fr>
2018-12-06 15:29:10 +0100

    Change start time

--- src/main/java/com/ahajri/hc/batch/config/BatchConfiguration.java
+++ src/main/java/com/ahajri/hc/batch/config/BatchConfiguration.java
@@ -132,7 +132,7 @@ public class BatchConfiguration {
 	}
 	// end::jobstep[]
 
-	@Scheduled(cron = "30 13 14 * * *")
+	@Scheduled(cron = "30 31 15 * * *")
 	public void startScandvPrayTimeJob() throws Exception {
 		LOG.info(" ====> Job Started at :" + new Date());
 		JobParameters param = new JobParametersBuilder().addString("JobID", String.valueOf(System.currentTimeMillis()))
